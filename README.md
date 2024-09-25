3b9604a95750daf950511137ab052de7888415b3
Tobias Hofmann <hofmann.tobias@outlook.com>
2024-09-25 15:33:03 +0200

    added test for agency VH

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Interactive-visual-regression-tests
-Sample Interactive visual regression tests for Fiori Elements app using BackstopJS
+
+## Step 2: Add tests to access homepage and open value help
+
+In this step the first visual regression tests are added. The first test is simply checking if the startpage of the app loads. The second test is to check the value help for filter value agency.
+
+Agency value help:
+
+![agency value help](backstop_data/bitmaps_reference/backstop_default_VH_Agency_0_document_0_desktop.png)
+
+### Configure agency value help test
+
+The following test scenario triggers a click on the value help of the agency filter and opens the value help dialog. Values to add to [backstop.json](backstop.json)
+
+```json
+{
+    "label": "VH Agency",
+    "url": "http://localhost:4004/travel_processor/webapp/index.html?sap-language=en",    
+    "clickSelectors": [
+    "#sap\\.fe\\.cap\\.travel\\:\\:TravelList--fe\\:\\:FilterBar\\:\\:Travel\\:\\:FilterField\\:\\:to_Agency_AgencyID-inner-vhi"
+    ]
+}
+```
+
+### Run test
+
+```sh
+npm test
+```
+
+### Create baseline
+
+```sh
+npx backstop approve
+```
---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,40 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Sflight_Startpage_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20240925-153106/backstop_default_Sflight_Startpage_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Sflight_Startpage_0_document_0_desktop.png",
+        "label": "Sflight Startpage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:4004/travel_processor/webapp/index.html?sap-language=en",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "error": "Reference file not found /Users/tobiashofmann/Documents/dev/code/testblog/visualtest/Interactive-visual-regression-tests/backstop_data/bitmaps_reference/backstop_default_Sflight_Startpage_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_VH_Agency_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20240925-153106/backstop_default_VH_Agency_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_VH_Agency_0_document_0_desktop.png",
+        "label": "VH Agency",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:4004/travel_processor/webapp/index.html?sap-language=en",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "error": "Reference file not found /Users/tobiashofmann/Documents/dev/code/testblog/visualtest/Interactive-visual-regression-tests/backstop_data/bitmaps_reference/backstop_default_VH_Agency_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
